a589f7408b14fa4ffbd357fb9aa4099835b8a59a
Mazzi2000 <adammaz00@o2.pl>
2025-12-01 13:17:56 -0600

    Adding a api route and retrieve data from the database and displaying it

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+from database import get_db_connection
 
 app = Flask(__name__)
+
+@app.route("/api/recipes")
+def get_data():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM recipes")
+    rows = cursor.fetchall()
+
+    connection.close()
+
+    recipes_list = []
+    for row in rows:
+        recipes_list.append(dict(row))
+
+    return jsonify(recipes_list)
 
 @app.route("/")
 def hello_adam():
